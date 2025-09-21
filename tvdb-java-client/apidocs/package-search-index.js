f033f833ee4ebd8a20496d2cbed04a749ce77aac
Andrew Miles <35545964+andy-miles@users.noreply.github.com>
2025-09-21 11:00:48 -0700

    tvdb-java-client 4.1.2

--- tvdb-java-client/apidocs/package-search-index.js
+++ tvdb-java-client/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.amilesend.tvdb.client"},{"l":"com.amilesend.tvdb.client.api"},{"l":"com.amilesend.tvdb.client.auth"},{"l":"com.amilesend.tvdb.client.model"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"com.amilesend.tvdb.client"},{"l":"com.amilesend.tvdb.client.api"},{"l":"com.amilesend.tvdb.client.auth"},{"l":"com.amilesend.tvdb.client.model"}];updateSearchResults();
